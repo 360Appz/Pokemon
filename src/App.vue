24594e2bbf4fe337e70df742325a6d2c3d910281
360Appz <cookiesbyjy@gmail.com>
2022-01-04 17:43:11 +0800

    Pokemon first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<!--<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Testing first vue app"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>-->
+<template>
+ <div id="app">
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pokemon
+  }
+}
+</script>
+
+<style lang="scss">
+ @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+#app {
+  body {
+     margin: 0;
+    padding: 0;
+  }
+  
+}
+</style>
+
+
+
+<!--
+<script>
+import Pokemon from './components/Pokemon.vue'
+export default {
+  components: {
+    Pokemon
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+  body {
+    margin: 0;
+    padding: 0;
+  }
+</style>-->
